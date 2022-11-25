772803f5aecd0fe872a6e65a3d19a0f3044f715b
gaston migone <gsmigone@gmail.com>
2022-11-25 15:59:53 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Data/PhotoSharingContext.cs
@@ -0,0 +1,18 @@
+﻿using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoSharingContext : DbContext
+    {
+        public PhotoSharingContext() : base("keyPhotoSharingDB")
+        { }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
